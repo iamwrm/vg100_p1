b816b2ffad057f79cd19ab0d4ae44b50750c2146
Ren Wang <iamwrm@gmail.com>
2017-05-27 21:05:59 +0800

    shuzui xie cuo le

--- p27_19/b3/b3.ino
+++ p27_19/b3/b3.ino
@@ -25,61 +25,61 @@ void setup()
 void loop()
 {
     digitalWrite(7, LOW);
-    digitalWrite(8f, HIGH);
+    digitalWrite(8, HIGH);
     digitalWrite(11, LOW);
     digitalWrite(12, HIGH);
 
     //---- before is just some setups ---------------
-    unsigned long t1[10, 2];
-    unsigned long t2[10, 2];
+    unsigned long t1[10][2];
+    unsigned long t2[10][2];
     // t1 is for moter 1
-    t1[0, 0] = t0; // time
-    t1[0, 1] = 0;  // velocity
-    t1[1, 0] = t0 + 1000 * 1;
-    t1[1, 1] = 50;
-    t1[2, 0] = t0 + 1000 * 2;
-    t1[2, 1] = 150;
-    t1[3, 0] = t0 + 1000 * 3;
-    t1[3, 1] = 200;
+    t1[0][0] = t0; // time
+    t1[0][1] = 0;  // velocity
+    t1[1][0] = t0 + 1000 * 1;
+    t1[1][1] = 50;
+    t1[2][0] = t0 + 1000 * 2;
+    t1[2][1] = 150;
+    t1[3][0] = t0 + 1000 * 3;
+    t1[3][1] = 200;
     for (int i = 4; i < 6; i++)
     {
-        t1[i, 0] = t0 + 1000 * i;
-        t1[i, 1] = 200;
+        t1[i][0] = t0 + 1000 * i;
+        t1[i][1] = 200;
     }
     for (int i = 6; i < 10; i++)
     {
-        t1[i, 0] = t0 + 1000 * i;
-        t1[i, 1] = 0;
+        t1[i][0] = t0 + 1000 * i;
+        t1[i][1] = 0;
     }
 
     // t2 is for moter 2
-    t2[0, 0] = t0; // time
-    t2[0, 1] = 0;  // velocity
-    t2[1, 0] = t0 + 1000 * 4;
-    t2[1, 1] = 50;
-    t2[2, 0] = t0 + 1000 * 5;
-    t2[2, 1] = 150;
-    t2[3, 0] = t0 + 1000 * 6;
-    t2[3, 1] = 200;
+    t2[0][0] = t0; // time
+    t2[0][1] = 0;  // velocity
+    t2[1][0] = t0 + 1000 * 4;
+    t2[1][1] = 50;
+    t2[2][0] = t0 + 1000 * 5;
+    t2[2][1] = 150;
+    t2[3][0] = t0 + 1000 * 6;
+    t2[3][1] = 200;
     for (int i = 4; i < 10; i++)
     {
-        t2[i, 0] = t0 + 1000 * i;
-        t2[i, 1] = 100;
+        t2[i][0] = t0 + 1000 * i;
+        t2[i][1] = 100;
     }
     //-----------------------
 
     // for moter 1
     int x = 0; // xiabiao
-    while (millis() < t1[x, 0])
+    while (millis() < t1[x][0])
     {
         x++;
     }
-    analogWrite(9, t1[x, 1]);
+    analogWrite(9, t1[x][1]);
     // for moter 2
     x = 0; // xiabiao
-    while (millis() < t2[x, 0])
+    while (millis() < t2[x][0])
     {
         x++;
     }
-    analogWrite(10, t2[x, 1]);
+    analogWrite(10, t2[x][1]);
 }
