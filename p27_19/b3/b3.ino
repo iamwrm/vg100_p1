d1e5e405fd2678bbb6343140bb8ab08158e561c7
Ren Wang <iamwrm@gmail.com>
2017-05-28 16:35:34 +0800

    aa

--- p27_19/b3/b3.ino
+++ p27_19/b3/b3.ino
@@ -42,22 +42,38 @@ void setup()
         t[1][i][1] = 0;
     }
 
+    //---------------------------------------------
+    //  xx      ----- moto 1
+    // x x 
+    //   x
+    //   x
+    // xxxx
     t[0][0][1] = 0; // velocity
-    for (char b = 0; b < 5 * 5; b++)
+    for (char b = 0 * 5; b < 5 * 5; b++)
     {
-        t[0][b][1] = b * 50 / 5;
+        t[0][b][1] = (b - 0 * 5) * 50 / 5;
     }
-    for (int b = 25; b < 30 * 5; b++)
+    for (int b = 5 * 5; b < 10 * 5; b++)
     {
-        t[0][b][1] = 20;
+        t[0][b][1] = 240;
     }
 
-    t[1][0][1] = 0; // velocity
-    for (char b = 0; b < 5 * 5; b++)
+    for (char b = 15 * 5; b < 20 * 5; b++)
     {
-        t[1][b][1] = b * 50 / 5;
+        t[0][b][1] = (b - 15 * 5) * 50 / 5;
     }
-    for (int b = 25; b < 30 * 5; b++)
+
+    // xxxxx      ----- moto 2
+    //     x
+    // xxxxx
+    // x
+    // xxxxx
+    t[1][0][1] = 0; // velocity
+    for (char b = 5 * 5; b < 10 * 5; b++)
+    {
+        t[1][b][1] = (b - 5 * 5) * 50 / 5;
+    }
+    for (int b = 10 * 5; b < 30 * 5; b++)
     {
         t[1][b][1] = 20;
     }
@@ -81,9 +97,7 @@ void loop()
             x++;
     }
     analogWrite(9, t[0][x][1]);
-    //--------debug
-    Serial.println(x);
-    //--------debug
+
     // for moter 2
     x = 0; // xiabiao
     while (millis() > t0 + (unsigned long)(t[1][x][0]))
@@ -93,9 +107,14 @@ void loop()
     }
     analogWrite(10, t[1][x][1]);
 
+    //--------debug
+    Serial.print(t[0][x][1]);
+    Serial.print("%");
+    Serial.println(t[1][x][1]);
+    //--------debug
+
     if (millis() > t0 + 1000 * 30)
     {
         delay(1000 * 500);
     }
 }
-
