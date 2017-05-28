7ae0bb203c078852888c9ddd1f3108fd91e81525
Ren Wang <iamwrm@gmail.com>
2017-05-28 15:54:56 +0800

    试着写速度

--- p27_19/b3/b3.ino
+++ p27_19/b3/b3.ino
@@ -1,9 +1,10 @@
 unsigned long t0;
 
-// total max time 30s 
-// frame 150 -> 1 s 5 frame 
+// total max time 30s
+// frame 150 -> 1 s 5 frame
 #define time_frame_max 150
 int t[2][time_frame_max][2];
+//  t[switch mot][time_frame][time stamp or velocity]
 
 void setup()
 {
@@ -30,38 +31,35 @@ void setup()
 
     // t[0] is for moter 1
     // 30 s real time
-    t[0][0][0] = 0; // time
-    t[0][0][1] = 0; // velocity
-    t[0][1][0] = 1000 * 1;
-    t[0][1][1] = 50;
-    t[0][2][0] = 1000 * 2;
-    t[0][2][1] = 150;
-    t[0][3][0] = 1000 * 3;
-    t[0][3][1] = 200;
-    for (int i = 4; i < 6; i++)
+    for (short i = 0; i < time_frame_max; i++)
     {
-        t[0][i][0] = 1000 * i;
-        t[0][i][1] = 200;
-    }
-    for (int i = 6; i < time_frame_max; i++)
-    {
-        t[0][i][0] = 1000 * i;
+        t[0][i][0] = time_frame_max * 1000 / 30;
         t[0][i][1] = 0;
     }
-
-    // t[1] is for moter 2
-    t[1][0][0] = 0; // time
-    t[1][0][1] = 0; // velocity
-    t[1][1][0] = 1000 * 4;
-    t[1][1][1] = 50;
-    t[1][2][0] = 1000 * 5;
-    t[1][2][1] = 150;
-    t[1][3][0] = 1000 * 6;
-    t[1][3][1] = 200;
-    for (int i = 4; i < time_frame_max; i++)
+    for (short i = 0; i < time_frame_max; i++)
     {
-        t[1][i][0] = 1000 * i;
-        t[1][i][1] = 100;
+        t[1][i][0] = time_frame_max * 1000 / 30;
+        t[1][i][1] = 0;
+    }
+
+    t[0][0][1] = 0; // velocity
+    for (char b = 0; b < 5 * 5; b++)
+    {
+        t[0][b][1] = b * 50 / 5;
+    }
+    for (int b = 25; b < 30 * 5; b++)
+    {
+        t[0][b][1] = 20;
+    }
+
+    t[1][0][1] = 0; // velocity
+    for (char b = 0; b < 5 * 5; b++)
+    {
+        t[1][b][1] = b * 50 / 5;
+    }
+    for (int b = 25; b < 30 * 5; b++)
+    {
+        t[1][b][1] = 20;
     }
 }
 
@@ -80,12 +78,13 @@ void loop()
     while (millis() > t0 + (unsigned long)(t[0][x][0]))
     {
         x++;
-
-        Serial.println("x");
-        Serial.println(x);
     }
     analogWrite(9, t[0][x][1]);
+
+    //--------debug
     Serial.println(t[0][x][1]);
+    //--------debug
+
     // for moter 2
     x = 0; // xiabiao
     while (millis() > t0 + (unsigned long)(t[1][x][0]))
@@ -93,13 +92,9 @@ void loop()
         x++;
     }
     analogWrite(10, t[1][x][1]);
+
+    if (millis() > t0 + 1000 * 30)
+    {
+        delay(1000 * 500);
+    }
 }
-
-
-
-
-
-
-
-
-
