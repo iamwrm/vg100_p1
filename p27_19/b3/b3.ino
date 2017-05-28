72a32af6f96595dee5f5680c2299f95cf99a2a3c
Ren Wang <iamwrm@gmail.com>
2017-05-28 15:14:42 +0800

    使用一个数组，开int 来减少内存

--- p27_19/b3/b3.ino
+++ p27_19/b3/b3.ino
@@ -1,7 +1,6 @@
 unsigned long t0;
 
-unsigned long t1[50][2];
-unsigned long t2[50][2];
+int t[2][50][2];
 
 void setup()
 {
@@ -26,39 +25,39 @@ void setup()
 
     //---- before is just some setups ---------------
 
-    // t1 is for moter 1
-    t1[0][0] = t0; // time
-    t1[0][1] = 0;  // velocity
-    t1[1][0] = t0 + 1000 * 1;
-    t1[1][1] = 50;
-    t1[2][0] = t0 + 1000 * 2;
-    t1[2][1] = 150;
-    t1[3][0] = t0 + 1000 * 3;
-    t1[3][1] = 200;
+    // t[0] is for moter 1
+    t[0][0][0] = 0; // time
+    t[0][0][1] = 0; // velocity
+    t[0][1][0] = 1000 * 1;
+    t[0][1][1] = 50;
+    t[0][2][0] = 1000 * 2;
+    t[0][2][1] = 150;
+    t[0][3][0] = 1000 * 3;
+    t[0][3][1] = 200;
     for (int i = 4; i < 6; i++)
     {
-        t1[i][0] = t0 + 1000 * i;
-        t1[i][1] = 200;
+        t[0][i][0] = 1000 * i;
+        t[0][i][1] = 200;
     }
     for (int i = 6; i < 50; i++)
     {
-        t1[i][0] = t0 + 1000 * i;
-        t1[i][1] = 0;
+        t[0][i][0] = 1000 * i;
+        t[0][i][1] = 0;
     }
 
-    // t2 is for moter 2
-    t2[0][0] = t0; // time
-    t2[0][1] = 0;  // velocity
-    t2[1][0] = t0 + 1000 * 4;
-    t2[1][1] = 50;
-    t2[2][0] = t0 + 1000 * 5;
-    t2[2][1] = 150;
-    t2[3][0] = t0 + 1000 * 6;
-    t2[3][1] = 200;
+    // t[1] is for moter 2
+    t[1][0][0] = 0; // time
+    t[1][0][1] = 0; // velocity
+    t[1][1][0] = 1000 * 4;
+    t[1][1][1] = 50;
+    t[1][2][0] = 1000 * 5;
+    t[1][2][1] = 150;
+    t[1][3][0] = 1000 * 6;
+    t[1][3][1] = 200;
     for (int i = 4; i < 50; i++)
     {
-        t2[i][0] = t0 + 1000 * i;
-        t2[i][1] = 100;
+        t[1][i][0] = 1000 * i;
+        t[1][i][1] = 100;
     }
 }
 
@@ -70,25 +69,25 @@ void loop()
     digitalWrite(11, LOW);
     digitalWrite(12, HIGH);
 
-        //-----------------------
+    //-----------------------
 
     // for moter 1
     int x = 0; // xiabiao
-    while (millis() > t1[x][0])
+    while (millis() > t0 + (unsigned long)(t[0][x][0]))
     {
         x++;
-        
+
         Serial.println("x");
         Serial.println(x);
     }
-    analogWrite(9, t1[x][1]);
-    Serial.println(t1[x][1]);
+    analogWrite(9, t[0][x][1]);
+    Serial.println(t[0][x][1]);
     // for moter 2
     x = 0; // xiabiao
-    while (millis() > t2[x][0])
+    while (millis() > t0 + (unsigned long)(t[1][x][0]))
     {
         x++;
     }
-    analogWrite(10, t2[x][1]);
+    analogWrite(10, t[1][x][1]);
 }
 
