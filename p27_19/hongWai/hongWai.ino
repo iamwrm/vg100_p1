12ca945807806d8f06bf515179ec4a3eaf547ca4
Ren Wang <iamwrm@gmail.com>
2017-06-11 20:33:39 +0800

    修正了一个重大bug，flad的设置为0 应该在setup中 loop中的分支 用 millis 来达到“delay”的效果

--- p27_19/hongWai/hongWai.ino
+++ p27_19/hongWai/hongWai.ino
@@ -1,3 +1,7 @@
+// author Wang Ren <iamwrm@gmail.com>
+int started_F, pulled_F, gone_F, relieved_F;
+long t0;
+
 void setup()
 {
     // initialize digital pin LED_BUILTIN as an output.
@@ -18,32 +22,31 @@ void setup()
     digitalWrite(11, LOW);
     digitalWrite(12, LOW);
 
-    //---- before is just some setups ---------------
-
-    // t[0] is for moter 1
-    // 30 s real time
-    Serial.print("b:");
+    started_F = 0;
+    pulled_F = 0;
+    gone_F = 0;
+    relieved_F = 0;
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-    //  analogWrite(9,200 );
-    //  analogWrite(10,200 );
     int obstacle = 0;
     obstacle = digitalRead(13);
     // have obstacle -> 0   on obstacle -> 1
     Serial.print(obstacle);
 
-    int started_F, pulled_F, gone_F, relieved_F;
-    started_F = 0;
-    pulled_F = 0;
-    gone_F = 0;
-    relieved_F = 0;
+    int dianJiPin = 10;
+    int duoJiPin = 9;
 
     if (started_F)
     {
-        // 拉电机
+        // 拉舵机
+        t0 = millis();
+        while (millis() < t0 + 3 * 1000)
+        {
+            analogWrite(duoJiPin, 100);
+        }
         pulled_F = 1;
         started_F = 0;
     }
@@ -51,12 +54,22 @@ void loop()
     if (pulled_F)
     {
         // 走
+        t0 = millis();
+        while (millis() < t0 + 3 * 1000)
+        {
+            analogWrite(dianJiPin, 100);
+        }
         gone_F = 1;
         pulled_F = 0;
     }
     if (gone_F)
     {
         // 放
+        t0 = millis();
+        while (millis() < t0 + 3 * 1000)
+        {
+            analogWrite(duoJiPin, -100);
+        }
         relieved_F = 1;
         gone_F = 0;
     }
